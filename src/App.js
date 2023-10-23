a1206979b0e6d7b9d10ef3edf301b2cd58b28a4b
WuanBui <118819560+WuanBui@users.noreply.github.com>
2023-10-23 20:53:45 +0700

    Submit E04

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CounterRow from "./components/CounterRow";
+import ProductRow from "./components/ProductRow";
+import ProductTable from "./components/ProductTable";
+import SearchBar from "./components/SearchBar";
+
+let counterRows = [];
+for (let i = 0; i < 5; i++) {
+  counterRows.push(<CounterRow Button1={i} id={i + 1} />);
+}
+
+// const [count, setCount] = React.useState(0);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <input type="text" id="fname" name="fname" placeholder="Search"></input>
+        <br></br>
+        <input
+          type="checkbox"
+          id="vehicle1"
+          name="vehicle1"
+          value="Bike"
+        ></input>
+        <label> Only show products in stock</label>
       </header>
-    </div>
+      <div id="counterContainer">
+        <table class="center">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <ProductTable/>
+            <ProductRow />
+            <ProductTable/>
+            <ProductRow />
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
